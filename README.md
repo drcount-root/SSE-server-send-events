3ab3a4616b57225a38ac0b6e94045f91c356d54d
Subham Sahu <about.subhamsahu@gmail.com>
2023-11-22 11:20:53 +0530

    SSE (server-send events)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# SSE-server-send-events
-SSE (server-send events)
+# SSE (server-send events)
+
+1. `npm start`
+
+2. Then open html file with live server extension.
---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  // Set up headers for SSE
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+  });
+
+  // Send initial event to the client
+  res.write("data: Initial event\n\n");
+
+  // Simulate sending updates every 5 seconds
+  const intervalId = setInterval(() => {
+    res.write(`data: Update at ${new Date().toLocaleTimeString()}\n\n`);
+  }, 5000);
+
+  // Close the connection after 25 seconds
+  setTimeout(() => {
+    clearInterval(intervalId);
+    res.end();
+  }, 25000);
+});
+
+server.listen(8000, () => {
+  console.log("Server listening on port 8000");
+});
