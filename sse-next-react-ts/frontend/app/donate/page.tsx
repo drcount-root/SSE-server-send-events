9f00ea8c55b411eec9afc913e80117edb431aa73
Subham Sahu <about.subhamsahu@gmail.com>
2023-11-28 16:13:10 +0530

    frontend done & api integrated

---
+++ sse-next-react-ts/frontend/app/donate/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+
+const DonatePage = () => {
+  const [amount, setAmount] = useState(777);
+
+  const donateFunction = async (e: FormEvent) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:4650/donate", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        "amount": amount,
+      }),
+    });
+
+    console.log(res, "post response");
+  };
+
+  return (
+    <div>
+      <form onSubmit={donateFunction}>
+        <label>Amount of donation</label>
+        <input
+          type="number"
+          name="amount"
+          value={amount}
+          placeholder="Enter amount"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setAmount(parseInt(e.target.value))
+          }
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default DonatePage;
---
+++ sse-next-react-ts/frontend/app/page.tsx
@@ -0,0 +1,43 @@
+'use client'
+import React, { useEffect, useState } from "react";
+
+type Donation = {
+  user: number;
+  amount: number;
+};
+
+export default function Home() {
+  const [donation, setDonation] = useState<Donation>({ user: 0, amount: 0 });
+
+  useEffect(() => {
+    const source = new EventSource(`http://localhost:4650/dashboard`);
+
+    source.addEventListener("open", () => {
+      console.log("SSE opened!");
+    });
+
+    source.addEventListener("message", (e) => {
+      console.log(e.data);
+      const data: Donation = JSON.parse(e.data);
+
+      setDonation(data);
+    });
+
+    source.addEventListener("error", (e) => {
+      console.error("Error: ", e);
+    });
+
+    return () => {
+      source.close();
+    };
+  }, []);
+
+  return (
+    <main>
+      <h1>Donation status</h1>
+      <hr />
+      <h3>Total amount: {donation.amount}</h3>
+      <h3>Total user: {donation.user}</h3>
+    </main>
+  );
+}
