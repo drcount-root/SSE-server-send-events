38b7bac745f5e1305bb38ec8314cdd845e21f989
Subham Sahu <about.subhamsahu@gmail.com>
2023-11-28 15:20:54 +0530

    backend implemented in sse-next-neact-ts

---
+++ sse-next-react-ts/ABOUT.md
@@ -0,0 +1,25 @@
+# The use case
+
+This is a minimalist real-time dashboard with a page that displays the total donation collected. Every time a new user makes a donation, the total of users and the donation amount will be updated without the page reload.
+
+## Backend Architecture
+
+The backend will have two endpoints:
+
+> 1. [POST] `/donate`: Make a donation
+> 2. [STREAM] `/dashboard`: Receive updates about the donation collected (GET)
+
+The frontend application will connect the endpoint /dashboard to receive updates.
+
+## Backend dependencies & starting up the backend
+
+>#### To Initialize Backend
+>1. `/backend$ npm init -y`
+>#### To install and initialize in the Typescript
+>2. `/backend$ npm install --save-dev typescript ts-node`
+>3. `/backend$ npx tsc --init`
+>#### To install Express framework and the package to configure CORS
+>4. `/backend$ npm install express cors`
+>5. `/backend$ npm install --save-dev @types/express @types/cors`
+>#### Backend Start up guide
+>6. `/backend$ npx ts-node src/app.ts`
--- sse-next-react-ts/backend/src/app.ts
+++ sse-next-react-ts/backend/src/app.ts
@@ -1,17 +1,48 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import cors from "cors";
 
 const donation = {
   user: 0,
-  amount: 0
+  amount: 0,
 };
 
 const app = express();
+const PORT = 4650;
+const SEND_INTERVAL = 2000;
 
 app.use(express.json());
 app.use(cors());
 
-app.post('/donate', (req, res) => {
+const writeEvent = (res: Response, sseId: string, data: string) => {
+  res.write(`id: ${sseId}\n`);
+  res.write(`data: ${data}\n\n`);
+};
+
+const sendEvent = (_req: Request, res: Response) => {
+  res.writeHead(200, {
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+    "Content-Type": "text/event-stream",
+  });
+
+  const sseId = new Date().toDateString();
+
+  setInterval(() => {
+    writeEvent(res, sseId, JSON.stringify(donation));
+  }, SEND_INTERVAL);
+
+  writeEvent(res, sseId, JSON.stringify(donation));
+};
+
+app.get("/dashboard", (req: Request, res: Response) => {
+  if (req.headers.accept === "text/event-stream") {
+    sendEvent(req, res);
+  } else {
+    res.json({ message: "Ok" });
+  }
+});
+
+app.post("/donate", (req, res) => {
   const amount = req.body.amount || 0;
 
   if (amount > 0) {
@@ -19,9 +50,9 @@ app.post('/donate', (req, res) => {
     donation.user += 1;
   }
 
-  return res.json({ message: 'Thank you ?'});
+  return res.json({ message: "Thank you for the donation! 😊" });
 });
 
-app.listen(4650, () => {
-  console.log(`Application started on URL ?`);
+app.listen(PORT, () => {
+  console.log(`Application started on URL : http://localhost:${PORT}`);
 });
